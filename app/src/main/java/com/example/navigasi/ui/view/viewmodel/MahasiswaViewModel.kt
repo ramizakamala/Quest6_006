a8675ebf7fd75cf18000dea5910f6fa95c141657
ramizakamala <mizarakamala@gmail.com>
2024-11-22 17:37:02 +0700

    Menambahkan import yang diperlukan untuk ViewModel dan StateFlow

---
+++ app/src/main/java/com/example/navigasi/ui/view/viewmodel/MahasiswaViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.navigasi.ui.view.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.navigasi.model.Mahasiswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+

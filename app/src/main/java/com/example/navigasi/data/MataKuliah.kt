15cb28c5a9cc3ee6c2ae2c6fc495a058991d4c0c
ramizakamala <mizarakamala@gmail.com>
2024-11-22 16:45:47 +0700

    menambahkan daftar mata kuliah dalam objek MataKuliah dan menambahkan object MataKuliah untuk menyimpan daftar mata kuliah yang tersedia

---
+++ app/src/main/java/com/example/navigasi/data/MataKuliah.kt
@@ -0,0 +1,12 @@
+package com.example.navigasi.data
+
+object MataKuliah {
+    val options = listOf(
+        "Pemrograman Aplikasi Mobile Lanjut",
+        "Pengembangan Aplikasi Web Framework",
+        "Prototyaping",
+        "Aplikasi Multimedia",
+        "Kecerdasan Buatan",
+        "Computer Vission"
+    )
+}

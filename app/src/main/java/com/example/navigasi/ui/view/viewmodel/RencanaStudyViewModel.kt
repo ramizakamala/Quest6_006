40c369928a83f551e4996d6f72da0cdaf7ba7d16
ramizakamala <mizarakamala@gmail.com>
2024-11-22 17:49:56 +0700

    Menambahkan ViewModel untuk Rencana Studi dan deklarasi state awal

---
+++ app/src/main/java/com/example/navigasi/ui/view/viewmodel/RencanaStudyViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.navigasi.ui.view.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.navigasi.model.RencanaStudi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class RencanaStudiViewModel : ViewModel() {
+    private val _krsState = MutableStateFlow(RencanaStudi())
+    val krsStateUi: StateFlow<RencanaStudi> = _krsState.asStateFlow()
+}
